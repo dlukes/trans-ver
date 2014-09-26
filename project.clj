cc9835455b154f0e533d2d2aba3494f6b935648f
David Lukes <dafydd.lukes@gmail.com>
2014-09-26 14:28:36 +0200

    v0.4.1-beta
    - let's test this for a while and if nothing goes wrong, this version will be
      sent out as the next official iteration

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject trans-ver "0.4.0"
+(defproject trans-ver "0.4.1-beta"
   :description "Transcription Verifier for spoken data collection at CNC [korpus.cz]"
   :url "http://github.com/dafydd-lukes/trans-ver"
   :license {:name "GPLv3"
